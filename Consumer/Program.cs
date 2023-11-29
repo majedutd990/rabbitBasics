358afa87ab0674da211c3fcb7d2c794ac0c67497
majed <majedutd@gmail.com>
2023-11-29 18:10:22 +0330

    repo for rabbit

---
+++ Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(
+    "letterbox",
+    false,
+    false,
+    false,
+    null);
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine("Received Message: {0}", message);
+};
+channel.BasicConsume("letterbox", true, consumer);
+Console.ReadKey();
---
+++ Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "guest",
+    Password = "guest"
+};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(
+    "letterbox",
+    false,
+    false,
+    false,
+    null);
+var message = "This is my first message";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+Console.WriteLine("We published a Message: {0}", message);
